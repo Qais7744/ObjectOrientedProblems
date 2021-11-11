2027b3088b4babd7abdeef1e35faf1d4b49bb7c9
unknown <altamashbagwan1@gmail.com>
2021-11-12 01:17:23 +0530

    Added: Fuctioning of Account Details

---
+++ src/com/bl/objectorientedproblem/TestingAccount.java
@@ -0,0 +1,8 @@
+package com.bl.objectorientedproblem;
+
+public class TestingAccount {
+    public static void main(String[] args) {
+        AccountList accountObj = new AccountList();
+        accountObj.mainOperation();
+    }
+}
